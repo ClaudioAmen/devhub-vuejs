05922ad2f1190758cb794ef4f80abc34db14b3a4
Claudio Amendola <claudio.amendola@elmecspa.it>
2025-06-27 08:38:11 +0200

    collegamento db

---
+++ express.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Prova funzionante");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://claudioamendola:IbTWowI6oecOMjNu@dbdevhub.axgloif.mongodb.net/?retryWrites=true&w=majority&appName=dbdevhub";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Questo è un pre compilato di MongoDb server per fare il collegamento alle chiamate API del database
---
+++ index.js
@@ -0,0 +1,75 @@
+// let variabile = 0
+// const costante = "" 
+// //var globale = "" ormai in disuso
+
+// variabile = "prova"
+
+// //costante = "ciao"
+
+// console.log("DevHub", variabile);
+
+//if (variabile == "ciao") {
+//    console.log("ciao")
+//} else {
+//    console.log(`la variabile contiene ${variabile}`)
+//};
+
+// switch (variabile) {
+//   case "0":
+//     console.log("zero");
+//     break;
+//   case "0":
+//     console.log("ciao");
+//         break;
+    
+//   default:
+//     break;
+// }
+// Utilizzando && serve per incatenare delle variabili ma le obbliga ad essere vere || anche i due slash verticali servono per concatenare delle variabili
+
+
+// let array = ['a', 'b', 'c']
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log('item',index, element)
+// }
+
+// facendo in questa maniera abbiamo indiciazzato un array e un ciclo for, in questo caso la const "element" è dichiarata solo dentro il ciclo
+// fosse stato dichiaro fuori avrebbe avuto lo scope globale , invece cosi è locale
+
+// let index = 0
+
+// while (index < array.length) {
+//     const element = array[index];
+//     console.log("item", index, element);
+//     index++
+// }
+
+// Questi sono i clicli standard e ci sono in tutti i linguaggi
+// js ha aggiunto il foreach , che server per eseguire un pezzo di codice e stampare gli elementi che ha all'interno di un array
+
+
+// array.forEach(element => {
+    
+// });
+
+// for (const element of object) {
+    
+// }
+
+// forof fa la stessa cosa ma deve essere usato in contesti diversi
+
+// let obj = {
+//   chiave: "valore",
+//   chiave2: "valore2",
+//   chiave3: "valore3",
+  
+// };
+
+// questo è un tipo di file in questo caso simuliamo un json , e server per dare dei dati da ciclare
+stringa = "ciao";
+
+let messaggio = stringa
+
+console.log(messaggio)
